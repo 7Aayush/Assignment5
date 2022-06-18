3fadadbac3410157fecdbccda58014ae3537a290
7Aayush <103490914+7Aayush@users.noreply.github.com>
2022-06-18 18:51:42 +0530

    Update Assignment 5.py

--- Assignment 5.py
+++ Assignment 5.py
@@ -87,6 +87,28 @@ for i in range(10):
     a_list.append(list_num)
 print(a_list)
 
+#part a:- (print +ve numbers)
+for num in a_list:
+    if num>0:
+        print(num," is a positive number")
+              
+#part b:- (print -ve numbers)
+for num in a_list:
+    if num<0:
+             print (num,'is a negative number')
+#part c:- (print odd numbers)
+for num in a_list:
+    if num%2!=0:
+                print (num,'is a odd number')
+#part d:- (print even numbers)
+for num in a_list:
+    if num%2==0:
+               print(num, 'is a even number')
+#part e- (print number of times each number occuring the list)
+for num in a_list:
+    count=a_list. count (num)
+    print (num,'occurs', count," times")
+
 
 #Ques9
 
